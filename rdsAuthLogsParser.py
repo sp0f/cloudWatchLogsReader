0e98b571f67e69263b28b4765f31aa51164d0423
Adam Łyjak <adam.lyjak@gmail.com>
2018-10-05 14:24:05 +0200

    sanity checks

--- rdsAuthLogsParser.py
+++ rdsAuthLogsParser.py
@@ -15,22 +15,21 @@ now=datetime.datetime.now(datetime.timezone.utc)
 kwargs = {
     'logGroupName': log_group,
     'limit': 10000,
-    'startTime' : None,
-    'filterPattern': '?FAILED_CONNECT ?CONNECT',
     'logStreamNamePrefix': 'dv-mysql.audit.log',
     'interleaved': True
 }
-if (path.exists(last_event_file)):
-    print("Reading last event from lock file")
-    tmp = f.readline()
-    last_event = tmp
-    kwargs['startTime'] = int(last_event[:13])+1
-    kwargs['startTime'] = last_event
-    os.remove(last_event_file)
 
-else:
-    kwargs['startTime'] = int(now.timestamp()*1000000)
-    last_event=str(kwargs['startTime'])
+last_event=int(now.timestamp()*1000000)
+# if (path.exists(last_event_file)):
+#     print("Reading last event from lock file")
+#     tmp = f.readline()
+#     last_event = tmp
+#     kwargs['startTime'] = int(last_event)+1
+#     os.remove(last_event_file)
+#
+# else:
+#     kwargs['startTime'] = int(now.timestamp()*1000000)
+#     last_event=str(kwargs['startTime'])
 
 
 # read logs in infinite loop
@@ -42,25 +41,28 @@ while True:
         timestamp, server_host, username, host, connection_id, query_id, operation, database, obj, retcode  = event['message'].split(",")
         event_time=datetime.datetime.fromtimestamp(int(timestamp)/1000000)
         log_line = event_time.strftime("%b %d %T") + " " + server_host + " rds: " + operation + " " + username + " " + host + " " + database + "\n"
+        print("Timestamp: " + str(timestamp))
         print(log_line)
         try:
             with  open(log_file, 'a') as f:
                 f.write(log_line)
+
             # just a simple sanity check for next log pull
             if last_event<timestamp:
                 last_event=timestamp
-            else:
-                print('last_event >= timestamp. It shouldn\'t happen')
+            elif last_event>timestamp:
+                print("WARNING: last_event > timestamp. It shouldn't happen")
             f.close()
         except:
-            print("Error while writing data to log. Exiting")
-            with open(last_event_file,'w') as f_event:
-                f.write(last_event)
+            print("ERROR: can't write data to log file. Exiting")
+            # with open(last_event_file,'w') as f_event:
+            #     f.write(last_event)
+            f.close()
             exit(1)
     try:
         kwargs['nextToken'] = resp['nextToken']
     except KeyError:
-        if last_event is not None:
-            kwargs['startTime']=int(last_event[:13])+1 # that's a tricky part, AWS returns 16 digit timestamp but expect 13 digits
-        print('No new log events available. Waiting 5s before next pull')
+        # if last_event is not None:
+        #     kwargs['startTime']=int(last_event)+1 # that's a tricky part, AWS returns 16 digit timestamp but expect 13 digits
+        # print('No new log events available. Waiting 5s before next pull')
         time.sleep(5) # no logs, let's wait
