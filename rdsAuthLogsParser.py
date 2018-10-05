f5b3f0d472e0be5aa6bc785bad86f27dbec350cf
Adam Łyjak <adam.lyjak@gmail.com>
2018-10-05 15:06:38 +0200

    sanity checks

--- rdsAuthLogsParser.py
+++ rdsAuthLogsParser.py
@@ -31,12 +31,13 @@ last_event=int(now.timestamp()*1000)
 #     kwargs['startTime'] = int(now.timestamp()*1000000)
 #     last_event=str(kwargs['startTime'])
 
-kwargs['startTime'] = last_event
 print("Start event timestamp: "+str(last_event))
+kwargs['startTime'] = last_event
+resp = client.filter_log_events(**kwargs)
+kwargs.pop('startTime')
 
 # read logs in infinite loop
 while True:
-    resp = client.filter_log_events(**kwargs)
     for event in resp['events']:
         print("Last event: "+str(last_event))
         # 1536652826127350,dv-mysql,alyjak,192.168.169.108,8426327,0,FAILED_CONNECT,,,1045
@@ -69,3 +70,4 @@ while True:
         #     kwargs['startTime']=int(last_event)+1 # that's a tricky part, AWS returns 16 digit timestamp but expect 13 digits
         # print('No new log events available. Waiting 5s before next pull')
         time.sleep(5) # no logs, let's wait
+        resp = client.filter_log_events(**kwargs)
